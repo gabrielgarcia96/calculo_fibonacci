b74afe255c6a9ac9cb4aa6ff04e16a434ce1457b
Gabrielgarcia <biel.garcia.mjv@gmail.com>
2023-03-07 15:43:26 -0300

    calculo_porcentagem_estados

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-function fibonacci(){
-    var termo = parseInt(document.getElementById('numero').value);
-    var resposta = document.getElementById('resposta');
-    var penultimo=0, ultimo=1;
-    var numero;
-  
-    if(termo<=2)
-     numero = termo-1;
-    else
-     for(var count=3 ; count<=termo ; count++){
-      numero = ultimo + penultimo;
-      penultimo = ultimo;
-      ultimo = numero;
-     }
-  
-    resposta.innerHTML=numero;
-  }
+var sp = 67.83643
+var rj = 36.67866
+var mg = 29.22988
+var es = 27.16548
+var out = 19.84953
+var total = (sp + rj + mg + es + out)
+console.log(total) 
+var psp = ((sp/total)*100)
+var prj = ((rj/total)*100)
+var pmg = ((mg/total)*100)
+var pes = ((es/total)*100)
+var pout = ((out/total)*100)
+
+console.log( `Porcentagem de SP {} ${psp}`);
+console.log( `Porcentagem de RJ {} ${prj}`);
+console.log( `Porcentagem de MG {} ${pmg}`);
+console.log( `Porcentagem de ES {} ${pes}`);
+console.log( `Porcentagem de OUT {} ${pout}`);
